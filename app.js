93e906b1c537c043a54acf620fe437b18d648783
Troy Chryssos <troychryssos@gmail.com>
2018-08-06 18:20:40 -0400

    allow cors

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // ROUTES
 const player = require('./routes/player.route');
